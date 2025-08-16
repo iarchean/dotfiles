26f3d96168ec9e7ff6ea158f74f12668ea580415
Archean Zhang <xu.zhang@linktivity.co.jp>
2025-08-16 10:30:20 +0900

    feat: enhance shell and process management configurations
    - Update btop config to version 1.4.4 and change default process sorting to "cpu direct"
    - Clarify disk filter documentation in btop config
    - Add shell integration for fish in ghostty config and two new keybindings for jumping to prompt
    - Add OrbStack integration and shell path detection to fish configuration
    - Add keybindings for jumping to previous and next prompts in tmux configuration
    
    Signed-off-by: Archean Zhang <xu.zhang@linktivity.co.jp>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -37,3 +37,9 @@ set -x XDG_CACHE_HOME "$HOME/.cache"
 # google cloud sdk
 set -x CLOUDSDK_CONFIG "$HOME/.gcloud"
 set -x GOOGLE_APPLICATION_CREDENTIALS "$CLOUDSDK_CONFIG/application_default_credentials.json"
+
+string match -q "$TERM_PROGRAM" "kiro" and . (kiro --locate-shell-integration-path fish)
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
